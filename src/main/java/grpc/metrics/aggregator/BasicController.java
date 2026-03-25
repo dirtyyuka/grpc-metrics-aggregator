373fc361878bd27601b037e71d0e5f99d077d587
mayank <fireboybusinesses@gmail.com>
2026-03-25 22:52:28 +0530

    file name typo

--- src/main/java/grpc/metrics/aggregator/BasicController.java
+++ src/main/java/grpc/metrics/aggregator/BasicController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.proto.tutorial.ProtoTutorial.Course;
 
 @RestController
-public class BasciController {
+public class BasicController {
     @Autowired
     CourseRepository courseRepository;
 
